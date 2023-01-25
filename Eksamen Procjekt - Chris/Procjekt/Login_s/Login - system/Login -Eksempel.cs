c712fd58b7e900834d5e46bca4e82b164a070007
AraAraSenpai <79848722+AraAraSenpai@users.noreply.github.com>
2023-01-25 09:24:06 +0100

    Add files via upload

---
+++ Eksamen Procjekt - Chris/Procjekt/Login_s/Login - system/Login -Eksempel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eksamen_Procjekt___Chris.Procjekt.Login_s.Login___system
+{
+    internal class login_eksempel
+    {
+        private void uaernames()
+        {
+            string username_c = "chris";
+        }
+        private void passwords()
+        {
+            string password_c = "chris";
+        }
+
+        public void login_system_eksempel(string password_c, string username_c)
+        {
+            int trys = 0;
+        }
+    }
+}
